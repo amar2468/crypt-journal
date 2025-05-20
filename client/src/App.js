f506ae43f7e87456499ea683f9dee0d3e214cdb5
amar2468 <plakaloam@gmail.com>
2025-05-20 21:10:37 +0100

    Added routes for two pages: one that will display "sign up/login" forms and the second one that displays the "forgot password" page.

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,16 @@ import '@fontsource/roboto/700.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import AuthPage from './pages/AuthPage';
+import ForgotPassword from './pages/ForgotPassword';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />} />
+        <Route path="/forgot_password" element={<ForgotPassword />} />
       </Routes>
     </Router>
   );
