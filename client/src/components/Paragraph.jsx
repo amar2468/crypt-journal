02ba9fac2cd080f4d08df66e26460606a3a85a93
amar2468 <plakaloam@gmail.com>
2025-05-20 21:35:15 +0100

    Removed the "text" prop in the "Paragraph" custom component and replaced it with "children" prop, meaning that the text needs to be passed between the "Paragraph" tags.

--- client/src/components/Paragraph.jsx
+++ client/src/components/Paragraph.jsx
@@ -1,9 +1,9 @@
 import Typography from '@mui/material/Typography';
 
-const Paragraph = ({ text, variant, align, sx }) => {
+const Paragraph = ({ children, variant, align, sx }) => {
     return (
         <Typography variant={variant} align={align} sx={sx}>
-            { text }
+            { children }
         </Typography>
     );
 };
