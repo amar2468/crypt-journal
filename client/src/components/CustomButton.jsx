80948aa940966c9eb7332991cddff73ccb6b6308
amar2468 <plakaloam@gmail.com>
2025-05-23 19:18:37 +0100

    Taking the prop that was passed from the page and linking it to the onclick function, so that it triggers a redirect to the specific page in question.

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,8 +1,12 @@
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
-const CustomButton = ({ children, variant, color="", size="", sx={}, startIcon=null }) => {
+const CustomButton = ({ children, variant, color="", size="", sx={}, startIcon=null, redirect_to="" }) => {
+
+    const navigate = useNavigate();
+
     return (
-        <Button variant={variant} color={color} size={size} sx={sx} startIcon={startIcon} >
+        <Button variant={variant} color={color} size={size} sx={sx} startIcon={startIcon} onClick={() => navigate(redirect_to)}>
             {children}
         </Button>
     );
