526b84c100af6e228050cc709a9701f93d94ef14
amar2468 <plakaloam@gmail.com>
2025-05-23 19:13:47 +0100

    Added routes for "terms and conditions" and "privacy policy"

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AuthPage from './pages/AuthPage';
 import ForgotPassword from './pages/ForgotPassword';
+import TermsAndConditions from './pages/TermsAndConditions';
+import PrivacyPolicy from './pages/PrivacyPolicy';
 
 function App() {
   return (
@@ -18,6 +20,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/forgot_password" element={<ForgotPassword />} />
+        <Route path="/terms_and_conditions" element={<TermsAndConditions />} />
+        <Route path="/privacy_policy" element={<PrivacyPolicy />} />
       </Routes>
     </Router>
   );
