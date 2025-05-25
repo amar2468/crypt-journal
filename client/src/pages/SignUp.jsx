92365cf85a0f554739ea0b2991b39958dc9c9e37
amar2468 <plakaloam@gmail.com>
2025-05-25 11:52:02 +0100

    Added a state variable to manage the input field values + handle change function that will update the values of the state variable + handle submit which will submit the form to the backend API.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -12,9 +12,64 @@ import Link from '@mui/material/Link';
 import ArrowBack from '@mui/icons-material/ArrowBack';
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
 import ToggleButton from '@mui/material/ToggleButton';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Alert from '@mui/material/Alert';
 
 
 const SignUp = ( { mode, switchForms } ) => {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    });
+
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleSubmit = async () => {
+        if (formData.password !== formData.confirmPassword) {
+                
+            setErrorMessage("The passwords entered do not match. Make sure the passwords match.")
+
+            return;
+        }
+
+        try {
+            const res = await axios.post('http://localhost:4000/api/auth/sign_up', formData);
+            console.log(res.data);
+            setErrorMessage('');
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+    };
+
+    let errorAlert;
+
+    if(errorMessage) {
+        errorAlert = (
+            <Alert variant="filled" severity="error" sx={{ width: "50%", margin: "0 auto", mt: 5 }}>
+                { errorMessage }
+            </Alert>
+        );
+    }
+
     return (
         <div>
             <Navbar />
@@ -49,7 +104,7 @@ const SignUp = ( { mode, switchForms } ) => {
 
                     sx={{ ml: 15, position: "absolute", mt: 5 }}
 
-                    redirect_to={-1}
+                    onClick={() => navigate(-1)}
                 >
 
                     Back
@@ -76,11 +131,16 @@ const SignUp = ( { mode, switchForms } ) => {
                 Sign up for a free account - with unlimited possibilities!
             </Paragraph>
 
+            {errorAlert}
+
             <Box display="flex" justifyContent="center" flexDirection="column" width="30%" sx={{ mt: 5, mx: "auto" }}>
                 <CustomInputField
                     label="First Name"
                     variant="outlined"
                     type="text"
+                    name="firstName"
+                    value={formData.firstName}
+                    onChange={handleChange}
                     sx={{ mt: 3 }}
                 />
 
@@ -88,6 +148,9 @@ const SignUp = ( { mode, switchForms } ) => {
                     label="Last Name"
                     variant="outlined"
                     type="text"
+                    name="lastName"
+                    value={formData.lastName}
+                    onChange={handleChange}
                     sx={{ mt: 3 }}
                 />
 
@@ -95,6 +158,9 @@ const SignUp = ( { mode, switchForms } ) => {
                     label="Email"
                     variant="outlined"
                     type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
                     sx={{ mt: 3 }}
                 />
 
@@ -102,6 +168,9 @@ const SignUp = ( { mode, switchForms } ) => {
                     label="Password"
                     variant="outlined"
                     type="password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleChange}
                     sx={{ mt: 3 }}
                 />
 
@@ -109,6 +178,9 @@ const SignUp = ( { mode, switchForms } ) => {
                     label="Confirm Password"
                     variant="outlined"
                     type="password"
+                    name="confirmPassword"
+                    value={formData.confirmPassword}
+                    onChange={handleChange}
                     sx={{ mt: 3 }}
                 />
 
@@ -141,7 +213,13 @@ const SignUp = ( { mode, switchForms } ) => {
                     sx={{ mt: 3 }} 
                 />
 
-                <CustomButton variant="contained" color="primary" size="large" sx={{ mt: 3}}>
+                <CustomButton
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    sx={{ mt: 3}}
+                    onClick={handleSubmit}
+                >
                     Sign Up
                 </CustomButton>
             </Box>
