25495d155e895ccedd3b574bf557994ed9d613df
amar2468 <plakaloam@gmail.com>
2025-06-07 13:37:49 +0100

    Check if user is logged in & update the state variable in any case + showing/hiding sign up, sign in, and logout buttons in navbar, depending on if user is logged in/out.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,10 +4,25 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Navbar = () => {
     const navigate = useNavigate();
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            setIsLoggedIn(true);
+        }
+        else {
+            setIsLoggedIn(false);
+        }
+    }, []);
 
     return (
         <Box sx={{ flexGrow: 1 }}>
@@ -23,13 +38,26 @@ const Navbar = () => {
                         Customer Support
                     </Button>
 
-                    <Button color="inherit" onClick={() => navigate('/auth?mode=sign_up')}>
-                        Sign Up
-                    </Button>
+                    {!isLoggedIn && (
+                        <>
+                            <Button color="inherit" onClick={() => navigate('/auth?mode=sign_up')}>
+                                Sign Up
+                            </Button>
 
-                    <Button color="inherit" onClick={() => navigate('/auth?mode=sign_in')}>
-                        Login
-                    </Button>
+                            <Button color="inherit" onClick={() => navigate('/auth?mode=sign_in')}>
+                                Login
+                            </Button>
+                        </>
+                    )}
+
+                    {isLoggedIn && (
+                        <Button color="inherit" onClick={() => {
+                            localStorage.removeItem("token");
+                            navigate('/auth?mode=sign_in');
+                        }}>
+                            LogOut
+                        </Button>
+                    )}
 
                 </Toolbar>
             </AppBar>
