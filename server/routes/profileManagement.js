7300d5cd63a48b10d3661a178a3d1ec98796cdd7
amar2468 <plakaloam@gmail.com>
2025-07-10 11:07:14 +0100

    Created a route file that will handle all requests from the account settings page. Currently, it just has a GET route which will retrieve the user's information, so that it can be populated in the account settings form.

---
+++ server/routes/profileManagement.js
@@ -0,0 +1,76 @@
+// Importing "express" module
+const express = require('express');
+
+// Create a new router instance for defining route handlers
+const router = express.Router();
+
+// Import middleware function to authenticate JWT tokens
+const { authenticateToken } = require('../middleware/verifyToken');
+
+// Importing the DB connection
+const db = require('../db');
+
+// Loads environment variables from .env file
+require('dotenv').config();
+
+// Getting the user information from the database, so that we can populate the account info with it.
+router.get('/getUserAccountInfo', authenticateToken, async(req, res) => {
+    // If the user is logged in, allow the retrieval of the user info
+    if (req.user) {
+
+        // Attempt to retrieve the user account and return the values from the database back to the frontend.
+        try
+        {
+            // Retrieve all the information for the user who is currently logged in.
+            const result = await db.query("SELECT * FROM users WHERE email=$1 LIMIT 1;", [req.user.email]);
+
+            // If the user was found, proceed with getting all the database fields and pass the values in the return statement.
+            if (result.rowCount > 0 ) {
+                // Retrieving a bunch of values from the database, that are needed in the account info form.
+                const first_name = result.rows[0].first_name;
+
+                const last_name = result.rows[0].last_name;
+
+                const email = result.rows[0].email;
+
+                const phone_number = result.rows[0].phone_number;
+
+                const mfa_enabled = result.rows[0].mfa_enabled;
+
+                // Returning a 200 status, along with a success message and the data that we want to populate the account info form with
+                return res.status(200).json({
+                    message: "User information retrieved successfully.",
+                    data: {
+                        first_name,
+                        last_name,
+                        email,
+                        phone_number,
+                        mfa_enabled
+                    }
+                });
+            }
+
+            // If there were no records found for that user, just send back a message informing the user about that.
+            else if (result.rowCount === 0) {
+                return res.status(404).json({
+                    message: "User not found."
+                });
+            }
+        }
+
+        // If there was an issue with retrieving the user information, inform the user about it.
+        catch
+        {
+            return res.status(500).json({
+                message: "Failed to retrieve user information."
+            });
+        }
+    }
+
+    // If the user is not logged in, don't allow the retrieval of the user info
+    else {
+        console.log("No token detected. Please contact the system administator for assistance.");
+    }
+});
+
+module.exports = router
