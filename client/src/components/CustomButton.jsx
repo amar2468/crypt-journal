d3095176fd5992a2058526bf80db1968e231676c
amar2468 <plakaloam@gmail.com>
2025-07-10 11:52:29 +0100

    Added a prop that identifies if the button is disabled.

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,9 @@
 import Button from '@mui/material/Button';
 
-const CustomButton = ({ children, variant, color="", size="", sx={}, startIcon=null, onClick={} }) => {
+const CustomButton = ({ children, variant, color="", size="", sx={}, startIcon=null, onClick={}, disabled=false }) => {
 
     return (
-        <Button variant={variant} color={color} size={size} sx={sx} startIcon={startIcon} onClick={onClick}>
+        <Button variant={variant} color={color} size={size} sx={sx} startIcon={startIcon} onClick={onClick} disabled={disabled}>
             {children}
         </Button>
     );
