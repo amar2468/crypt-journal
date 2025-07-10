0ce92aee880a288260f7fa780a504fd7de7773c8
amar2468 <plakaloam@gmail.com>
2025-07-10 11:08:57 +0100

    Added useEffect that makes an API call to retrieve user information upon page load + function that updates the form data when the user changes the fields in the form + added error alerts.

--- client/src/pages/Settings.jsx
+++ client/src/pages/Settings.jsx
@@ -10,18 +10,135 @@ import Tabs from '@mui/material/Tabs';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Paragraph from '../components/Paragraph';
 import { useState } from 'react';
+import { useEffect } from 'react';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Alert from '@mui/material/Alert';
 
 const Settings = () => {
+    const navigate = useNavigate();
+
     // State variable which represents the menu tab number (account info (0), preferences (1), delete your account (2))
     const [tabNumber, setTabNumber] = useState(0);
+
+    // State variable representing if the user is authorised to view this page.
+    const [authChecked, setAuthChecked] = useState(false);
 
     // State variable which represents the timezone
     const [timezone, setTimezone] = useState("Europe/Dublin");
 
     // State variable which represents the date format
     const [dateFormat, setDateFormat] = useState("DD/MM/YYYY");
+
+    const [accountInfoForm, setAccountInfoForm] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        mfaEnabled: false
+    });
+
+    // State variable that shows error alerts, if something isn't right.
+    const [errorMessage, setErrorMessage] = useState("");
+
+    // When the page is loaded, we will retrieve the user's token and make an API call to the backend,
+    // to retrieve the user's information and populate the settings page with those details.
+    useEffect(() => {
+        // Retrieve the token from localStorage (set after login)
+        const token = localStorage.getItem("token");
+        
+        // If the token exists for this user, proceed with the needful.
+        if (token) {
+            // Setting this to true means that we can show the contents of this page to the user (user is authorised.)
+            setAuthChecked(true);
+            
+            // Make an API GET call to retrieve the user details, passing the token to the backend.
+            const getUserData = async () => {
+                const userDetails = await axios.get("http://localhost:4000/api/profileManagement/getUserAccountInfo", {
+                    headers: {
+                        Authorization: `Bearer ${token}`
+                    }
+                });
+
+                setErrorMessage("");
+
+                // If the user information was successfully retrieved, we will update the form with those details
+                if (userDetails.status === 200) {
+                    // Populate the account information form with the details for this specific user (retrieved from the API call above.)
+                    setAccountInfoForm(prev => ({
+                        ...prev,
+                        firstName: userDetails.data.data.first_name,
+                        lastName: userDetails.data.data.last_name,
+                        email: userDetails.data.data.email,
+                        phoneNumber: userDetails.data.data.phone_number,
+                        mfaEnabled: userDetails.data.data.mfa_enabled
+                    }));
+                }
+
+                // If the user couldn't be found, we will display that message to the user
+                else if (userDetails.status === 404) {
+                    setErrorMessage("Your user account information couldn't be found. Please raise this request with our support team.");
+                    
+                    return;
+                }
+
+                // If there was an internal server error, we will display that message to the user.
+                else if (userDetails.status === 500) {
+                    setErrorMessage("Internal server error - Please raise this request with our support team.");
+
+                    return;
+                }
+            };
+
+            // After we check to see that the user is authorised, we will call the function above that makes the API call.
+            getUserData();
+        }
+
+        // If the user is not authorised, we will redirect to the main page.
+        else {
+            navigate("/")
+        }
+    }, [navigate]);
+
+    // If the user is not authorised, do not show anything on the page.
+    if (!authChecked) {
+        return null;
+    }
+
+    // When an update is made to the account information form, this will be executed.
+    const updateAccountInfoForm = (event) => {
+        // Getting the field name that was updated
+        const name = event.target.name;
+
+        // Getting the field value that was updated.
+        const value = event.target.value;
+        
+        // Checking to see if MFA is enabled
+        const mfaIsEnabled = event.target.checked;
+
+        // If the change was to the MFA, we will update the MFA toggle with the new info.
+        if (event.target.type === "checkbox") {
+            setAccountInfoForm(prev => ({
+                ...prev,
+                [name]: mfaIsEnabled
+            }));
+        }
+
+        // If the change was made to everything excluding the MFA, we will update it with the new info.
+        else {
+            setAccountInfoForm(prev => ({
+                ...prev,
+                [name]: value
+            }));
+        }
+
+    };
+
+    const submitAccountInfoForm = async () => {
+        
+    };
 
     // Most common timezones added into the list
     const timezones = [
@@ -50,6 +167,17 @@ const Settings = () => {
             'aria-controls': `vertical-tabpanel-${tabIndex}`,
         };
     };
+
+    let errorAlert;
+
+    // If an error message was detected, it will be displayed in the form of an alert message.
+    if (errorMessage) {
+        errorAlert = (
+            <Alert variant="filled" severity="error" sx={{ width: "50%", margin: "0 auto", mt: 5 }}>
+                { errorMessage }
+            </Alert>
+        );
+    }
     
     return (
         <div>
@@ -101,6 +229,12 @@ const Settings = () => {
 
                             type="text"
 
+                            name="firstName"
+
+                            value={accountInfoForm.firstName}
+
+                            onChange={updateAccountInfoForm}
+
                             sx={{ mt: 3 }}
                         />
 
@@ -110,6 +244,12 @@ const Settings = () => {
                             variant="outlined"
 
                             type="text"
+
+                            name="lastName"
+
+                            value={accountInfoForm.lastName}
+
+                            onChange={updateAccountInfoForm}
 
                             sx={{ mt: 3 }}
                         />
@@ -121,6 +261,12 @@ const Settings = () => {
 
                             type="email"
 
+                            name="email"
+
+                            value={accountInfoForm.email}
+
+                            onChange={updateAccountInfoForm}
+
                             sx={{ mt: 3 }}
                         />
 
@@ -131,10 +277,30 @@ const Settings = () => {
 
                             type="tel"
 
+                            name="phoneNumber"
+
+                            value={accountInfoForm.phoneNumber}
+
+                            onChange={updateAccountInfoForm}
+
                             sx={{ mt: 3 }}
                         />
 
-                        <FormControlLabel control={<Switch />} label="Enable Multi-Factor Authentication (MFA)" sx={{ mt: 3 }} />
+                        <FormControlLabel
+                        
+                            control={<Switch />}
+                            
+                            label="Enable Multi-Factor Authentication (MFA)"
+
+                            name="mfaEnabled"
+
+                            value={accountInfoForm.mfaEnabled}
+
+                            onChange={updateAccountInfoForm}
+                            
+                            sx={{ mt: 3 }}
+                            
+                        />
 
                         <CustomButton
                             variant="contained"
@@ -144,6 +310,8 @@ const Settings = () => {
                             size="large"
 
                             sx={{ mt: 3 }}
+
+                            onClick={submitAccountInfoForm}
                         >
                             Save
                         </CustomButton>
